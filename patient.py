5efa4f82161f84134089bd3fb94a07c17c8d860b
Julio <juliopradom@gmail.com>
2021-06-09 00:35:34 +0200

    updating patient

--- patient.py
+++ patient.py
@@ -1,19 +1,32 @@
-from PIL import Image
 import cv2
-from numpy import asarray
-import numpy
 import pywt
 import numpy as np
 
 GROUPS = ["ad", "cn", "emci", "mci", "lmci", "smc"]
 SLICES = [55, 56, 61, 72, 82, 90, 104, 106, 114]
-COEFF_DETAILS = {"ad", "da", "dd"}
 PATH_TO_FOLDER = "./images"
 IMAGE_FORMAT = "{group}_wc1_{patient_number}_{slice_number}.jpg"
 
 class Patient:
     
-    def __init__(self, patient_number, group, path_to_folder=PATH_TO_FOLDER, normalize=True):
+    def __init__(
+        self, 
+        patient_number, 
+        group, 
+        path_to_folder=PATH_TO_FOLDER, 
+        normalize_colour=True, 
+        resize_image=False
+    ):
+        """ Constructor: initialize an instance of the class.
+        
+        Args:
+            patient_number (str): number of the patient
+            group (str): group to which the patient belongs
+            path_to_folder (str): relative path to images folder
+            normalize_colour (str): if set to True the image will be 
+                                    converted to BGR2GRAY
+            resize_image (str): if set to True the image will be resized to 224x224
+       """
         if group.lower() not in GROUPS:
             raise Exception(f"Error: group={group} not valid")
         group = group.upper()
@@ -22,41 +35,67 @@ class Patient:
             image_file_name = IMAGE_FORMAT.format(group=group, 
                                                   patient_number=patient_number, 
                                                   slice_number=slice_number
-                                                  )
+                                            )
             path_to_slice = f"{path_to_folder}/{group}/{image_file_name}"
             try:
                 image = cv2.imread(path_to_slice)
-                if normalize:
-                    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                else:
+                if resize_image:
                     image.resize((224,224,3)) 
+                if normalize_colour:
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                 image = np.float32(image)
                 image /= 255
                 images[str(slice_number)] = image 
             except Exception as e:
-                raise Exception(f"Error: could not load slice={slice_number} \
-                                for patient={patient_number}, detail={e}") 
+                raise Exception(f"Error: could not load slice={slice_number}" \
+                                f"for patient={patient_number}, detail={e}") 
         self.images = images
-        self.normalized = normalize
+        self.normalized_colour = normalize_colour
         self.group = group.lower()
     
     def get_full_images_array(self):
+        """ Gets the all the flatten images array concatenated in a single 1D array
+        Returns:
+            np.array 
+       """
         
         final_image = self.images[str(SLICES[0])]
         for slice_number in SLICES[1:]:
-            final_image = np.append(final_image, self.images[str(slice_number)], axis=0)
+            final_image = np.append(
+                                final_image, 
+                                self.get_single_image_array(slice_number), 
+                                axis=0
+                            )
         return final_image
     
     def get_single_image_array(self, slice_number):
-        
+        """ Gets the flatten array corresponding to the slice specified in the 
+            parameter
+        Args:
+            slice_number (int): number of the slice to access
+        Returns:
+            np.array 
+       """
         return self.images[str(slice_number)]
     
     def get_slice_wavelet_coefficients(self, wavelet_name, level, slice_number):
-        
-        if not self.normalized:
+        """ Gets the wavelet coefficients associated with the slice_number 
+            specified in the parameter
+        Args:
+            wavelet_name (str): wavelet type to use
+            level (int): level (depth) of the approximation layer to access
+            slice_number (int): number of the slice to access    
+        Returns:
+            list
+       """
+        if not self.normalized_colour:
             raise Exception("Error: patient's images are not normalized")
         # Gets only approx layer in the specified level
-        raw_coeffs = pywt.wavedec2(data=self.images[str(slice_number)], wavelet=wavelet_name, level=level)
+        raw_coeffs = pywt.wavedec2(
+                            data=self.get_single_image_array(slice_number), 
+                            wavelet=wavelet_name, 
+                            level=level
+                        )
         target_coeffs = raw_coeffs[0]
         coeffs = []
         for i in range(len(target_coeffs)):
@@ -66,12 +105,22 @@ class Patient:
         return coeffs
     
     def get_wavelet_coefficients(self, wavelet_name, level):
-        
-        if not self.normalized:
+        """ Gets all the wavelet coefficients associated with the patient
+        Args:
+            wavelet_name (str): wavelet type to use
+            level (int): level (depth) of the approximation layer to access         
+        Returns:
+            list
+       """
+       
+        if not self.normalized_colour:
             raise Exception("Error: patient's images are not normalized")
         coefficients_list = []
         for slice_number in self.images:
-            coefficients_item = self.get_slice_wavelet_coefficients(wavelet_name, level, slice_number)
-            coefficients_list = numpy.concatenate([coefficients_list, coefficients_item])
+            coefficients_item = self.get_slice_wavelet_coefficients(
+                                    wavelet_name, 
+                                    level, 
+                                    slice_number
+                                )
+            coefficients_list = np.concatenate([coefficients_list, coefficients_item])
         return coefficients_list
-    
